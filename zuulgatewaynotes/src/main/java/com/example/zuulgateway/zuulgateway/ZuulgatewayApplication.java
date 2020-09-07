656e302d4355fa549d5738c762738eb7cb4b0176
HP <HP@Praveen-hp>
2020-09-07 11:50:30 +0530

    Zuul Gateway for microservices

---
+++ zuulgatewaynotes/src/main/java/com/example/zuulgateway/zuulgateway/ZuulgatewayApplication.java
@@ -0,0 +1,20 @@
+package com.example.zuulgateway.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient 
+public class ZuulgatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulgatewayApplication.class, args);
+	}
+	
+}
